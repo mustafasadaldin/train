fee9f71bd0bb987e93e37c7ac10d80964ff71712
mustafa sadaldin <HP@mustafasaadaldeen.com>
2023-02-25 16:20:41 +0200

    final Updates

--- src/main/java/com/example/demo/services/UsersService.java
+++ src/main/java/com/example/demo/services/UsersService.java
@@ -5,7 +5,10 @@ import com.example.demo.models.User;
 public interface UsersService {
 
      User insert(User users);
+
      void deleteUser(int id);
+
      User getUser(int id);
+
      User updateUser(int id, User user);
 }
